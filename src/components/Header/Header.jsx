e3d70988ae014f7f18eb3d95a30534577280f61d
Przemek <przemyslaw.gorecki93@gmail.com>
2023-04-12 20:50:59 +0200

    Add logo on Home Page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../../assets/logo_no_bg.png';
+
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <img src={logo} className="w-1/2" />
+      <h1 className="text-5xl">Page in construction</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { StoryblokComponent, storyblokEditable } from '@storyblok/react';
+import Header from '../components/Header/Header';
 
 const Home = ({ blok }) => (
   <main {...storyblokEditable(blok)}>
-    <h1>Page in construction</h1>
+    <Header />
     {blok.body ? blok.body.map((blok) => <StoryblokComponent blok={blok} key={blok._uid} />) : null}
   </main>
 );
