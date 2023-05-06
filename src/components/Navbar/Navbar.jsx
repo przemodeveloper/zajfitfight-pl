79d3f0c0cc2b02c14b8c8441f138cc012269097e
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-06 14:25:01 +0200

    Change to modules

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Twirl as Hamburger } from 'hamburger-react';
-import './Navbar.scss';
+import styles from './Navbar.module.scss';
 import Logo from '../Logo/Logo';
 
 const Navbar = () => {
@@ -12,53 +12,55 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
+    <nav>
       <div className="flex justify-between h-full">
         <Logo />
-        <div className="menu-icon p-2">
+        <div className={`${styles.hamburger} p-2`}>
           <Hamburger toggled={showNavbar} toggle={handleShowNavbar} color="#cf142b" size={32} />
         </div>
         <div
-          className={`nav-elements  ${
-            showNavbar && 'active'
+          className={`${styles.elements}  ${
+            showNavbar && styles.active
           } text-2xl text-white bg-secondary  border-b-4 border-l-4 border-primary`}>
           <ul className="flex justify-between px-12 py-8 gap-x-4 lg:gap-x-8">
             <li>
-              <NavLink className={({ isActive }) => (isActive ? 'active-link' : undefined)} to="/">
+              <NavLink
+                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+                to="/">
                 Strona główna
               </NavLink>
             </li>
             <li>
               <NavLink
-                className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
                 to="galeria">
                 Galeria
               </NavLink>
             </li>
             <li>
               <NavLink
-                className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
                 to="o-nas">
                 O nas
               </NavLink>
             </li>
             <li>
               <NavLink
-                className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
                 to="grafik">
                 Grafik
               </NavLink>
             </li>
             <li>
               <NavLink
-                className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
                 to="cennik">
                 Cennik
               </NavLink>
             </li>
             <li>
               <NavLink
-                className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
                 to="kontakt">
                 Kontakt
               </NavLink>
