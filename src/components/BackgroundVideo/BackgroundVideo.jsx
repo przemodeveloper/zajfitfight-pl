12eaaadab4b0cb22285ed5b0c8eb74f0b6f02c56
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-07 13:30:34 +0200

    Use useEffect for play video

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useRef } from 'react';
 import backgroundVideo from '../../assets/video.mp4';
 import styles from './BackgroundVideo.module.scss';
 
 const BackgroundVideo = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    videoRef.current.play();
+  }, []);
+
   return (
-    <video muted autoPlay playsinline loop className={styles.video}>
+    <video muted loop className={styles.video} ref={videoRef}>
       <source src={backgroundVideo} type="video/mp4" />
     </video>
   );
