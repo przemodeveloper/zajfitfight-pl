c9c11256ebb3d30ef6095c7cc87d9c250cb14089
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-13 09:40:02 +0200

    Add tweaks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,61 +12,59 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
-      <div className="flex justify-between h-full bg-secondary border-primary border-b-4">
-        <Logo />
-        <div className={`${styles.hamburger} p-2`}>
-          <Hamburger toggled={showNavbar} toggle={handleShowNavbar} color="#cf142b" size={32} />
-        </div>
-        <div
-          className={`${styles.elements}  ${
-            showNavbar && styles.active
-          } text-3xl text-white bg-secondary border-l-4 md:border-l-0 border-primary`}>
-          <ul className="flex justify-between md:items-center px-12 py-8 gap-x-4 lg:gap-x-8">
-            <li onClick={() => setShowNavbar(false)}>
-              <NavLink
-                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
-                to="/">
-                Strona główna
-              </NavLink>
-            </li>
-            <li onClick={() => setShowNavbar(false)}>
-              <NavLink
-                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
-                to="galeria">
-                Galeria
-              </NavLink>
-            </li>
-            <li onClick={() => setShowNavbar(false)}>
-              <NavLink
-                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
-                to="o-nas">
-                O nas
-              </NavLink>
-            </li>
-            <li onClick={() => setShowNavbar(false)}>
-              <NavLink
-                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
-                to="grafik">
-                Grafik
-              </NavLink>
-            </li>
-            <li onClick={() => setShowNavbar(false)}>
-              <NavLink
-                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
-                to="cennik">
-                Cennik
-              </NavLink>
-            </li>
-            <li onClick={() => setShowNavbar(false)}>
-              <NavLink
-                className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
-                to="kontakt">
-                Kontakt
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+    <nav className="flex justify-between h-full bg-secondary border-primary border-b-4">
+      <Logo />
+      <div className={`${styles.hamburger} mr-2`}>
+        <Hamburger toggled={showNavbar} toggle={handleShowNavbar} color="#cf142b" size={32} />
+      </div>
+      <div
+        className={`${styles.elements}  ${
+          showNavbar && styles.active
+        } text-3xl text-white bg-secondary border-l-4 md:border-l-0 border-primary`}>
+        <ul className="flex justify-between md:items-center px-12 py-8 gap-x-4 lg:gap-x-8">
+          <li onClick={() => setShowNavbar(false)}>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+              to="/">
+              Strona główna
+            </NavLink>
+          </li>
+          <li onClick={() => setShowNavbar(false)}>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+              to="galeria">
+              Galeria
+            </NavLink>
+          </li>
+          <li onClick={() => setShowNavbar(false)}>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+              to="o-nas">
+              O nas
+            </NavLink>
+          </li>
+          <li onClick={() => setShowNavbar(false)}>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+              to="grafik">
+              Grafik
+            </NavLink>
+          </li>
+          <li onClick={() => setShowNavbar(false)}>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+              to="cennik">
+              Cennik
+            </NavLink>
+          </li>
+          <li onClick={() => setShowNavbar(false)}>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles['active-link'] : undefined)}
+              to="kontakt">
+              Kontakt
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
