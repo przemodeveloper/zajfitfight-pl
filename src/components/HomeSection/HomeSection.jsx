84dd7fffc2936f845ed4f574c16e25ef9b6da065
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-13 12:14:20 +0200

    Tweak

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -2,8 +2,8 @@ import { storyblokEditable } from '@storyblok/react';
 import styles from './HomeSection.module.scss';
 
 const HomeSection = ({ blok }) => (
-  <div {...storyblokEditable(blok)} className={styles.home}>
-    <div className={styles.title} data-aos="fade-right">
+  <div {...storyblokEditable(blok)} className={styles.title}>
+    <div data-aos="fade-right">
       <h1 className="text-8xl md:text-9xl text-white">{blok.title}</h1>
     </div>
   </div>
