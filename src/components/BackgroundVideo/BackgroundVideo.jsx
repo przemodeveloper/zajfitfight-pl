7dc2139796b9eb2f19c89889193118adeac81789
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-16 19:34:15 +0200

    Add playsInline

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -1,16 +1,9 @@
-import { useEffect, useRef } from 'react';
 import backgroundVideo from '../../assets/video.mp4';
 import styles from './BackgroundVideo.module.scss';
 
 const BackgroundVideo = () => {
-  const videoRef = useRef();
-
-  useEffect(() => {
-    videoRef.current.play();
-  }, []);
-
   return (
-    <video muted loop className={styles.video} ref={videoRef}>
+    <video muted autoPlay playsInline loop className={styles.video}>
       <source src={backgroundVideo} type="video/mp4" />
     </video>
   );
