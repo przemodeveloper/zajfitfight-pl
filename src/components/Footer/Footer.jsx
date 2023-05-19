17d3d5bbd0835fb9d90b214604a33a783f187621
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-19 07:47:07 +0200

    Decrease font size

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ import { UilLinkedin } from '@iconscout/react-unicons';
 const Footer = () => {
   return (
     <div
-      className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-xl`}>
+      className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-lg`}>
       <p>© 2023 Zajfit&Fight, All Rights Reserved</p>
       <span className="flex">
         <p className="mr-2">Created by Przemek Górecki</p>
         <a href="https://www.linkedin.com/in/przemyslaw-gorecki/" rel="noreferrer" target="_blank">
-          <UilLinkedin />
+          <UilLinkedin className="w-3/4" />
         </a>
       </span>
     </div>
