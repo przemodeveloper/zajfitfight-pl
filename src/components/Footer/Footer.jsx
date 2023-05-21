14dbcf1f73c70504c3b610b56469acc3d795f3f9
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-21 14:30:10 +0200

    Change footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,11 @@
 import styles from './Footer.module.scss';
-// import { UilLinkedin } from '@iconscout/react-unicons';
 
 const Footer = () => {
   return (
-    <div
+    <footer
       className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-base md:text-xl`}>
       <p>© 2023 Zajfit&Fight, All Rights Reserved</p>
-      {/* <span className="flex">
-        <p className="mr-2">Created by Przemek Górecki</p>
-        <a href="https://www.linkedin.com/in/przemyslaw-gorecki/" rel="noreferrer" target="_blank">
-          <UilLinkedin className="w-3/4" />
-        </a>
-      </span> */}
-    </div>
+    </footer>
   );
 };
 
