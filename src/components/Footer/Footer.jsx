7b8b19add5bacded697f745d9399706e3331b02e
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-21 12:20:51 +0200

    Add tweaks to cookie banner

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <div
-      className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-lg md:text-xl`}>
+      className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-base md:text-xl`}>
       <p>© 2023 Zajfit&Fight, All Rights Reserved</p>
       {/* <span className="flex">
         <p className="mr-2">Created by Przemek Górecki</p>
