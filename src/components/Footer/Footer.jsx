7e8a3bc8d9ce319fbf2cd042603d4588b8aa8038
Przemek <przemyslaw.gorecki93@gmail.com>
2023-05-21 12:07:43 +0200

    Remove creator

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import styles from './Footer.module.scss';
-import { UilLinkedin } from '@iconscout/react-unicons';
+// import { UilLinkedin } from '@iconscout/react-unicons';
 
 const Footer = () => {
   return (
     <div
       className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-lg md:text-xl`}>
       <p>© 2023 Zajfit&Fight, All Rights Reserved</p>
-      <span className="flex">
+      {/* <span className="flex">
         <p className="mr-2">Created by Przemek Górecki</p>
         <a href="https://www.linkedin.com/in/przemyslaw-gorecki/" rel="noreferrer" target="_blank">
           <UilLinkedin className="w-3/4" />
         </a>
-      </span>
+      </span> */}
     </div>
   );
 };
