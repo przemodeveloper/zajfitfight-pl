416e76a18d24b35defa0071ab8b72fd2f85927a1
Przemek <przemyslaw.gorecki93@gmail.com>
2023-06-07 20:41:33 +0200

    Tweak margin

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import { storyblokEditable } from '@storyblok/react';
 
 const Header = ({ blok }) => (
-  <div {...storyblokEditable(blok)} className="mt-24 md:mt-16">
-    <h1 className="text-6xl text-white">{blok.title}</h1>
+  <div {...storyblokEditable(blok)} className="mt-8 md:mt-16">
+    <h1 className="text-5xl md:text-6xl text-white">{blok.title}</h1>
   </div>
 );
 
