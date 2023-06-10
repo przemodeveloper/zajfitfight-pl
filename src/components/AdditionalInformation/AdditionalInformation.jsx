156385ac1ce14e66c334ab34abe5c2815d8323a6
Przemek <przemyslaw.gorecki93@gmail.com>
2023-06-10 09:54:16 +0200

    Add prewrap to additional information

--- src/components/AdditionalInformation/AdditionalInformation.jsx
+++ src/components/AdditionalInformation/AdditionalInformation.jsx
@@ -2,7 +2,7 @@ import { storyblokEditable } from '@storyblok/react';
 
 const AdditionalInformation = ({ blok }) => (
   <div {...storyblokEditable(blok)} className="mt-8 md:mt-16">
-    <p className="text-2xl md:text-3xl text-white">{blok.text}</p>
+    <p className="text-2xl md:text-3xl text-white whitespace-pre-wrap">{blok.text}</p>
   </div>
 );
 
