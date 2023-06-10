2b5fe77337d9c11e06b5039ec12a5508de9a9aa1
Przemek <przemyslaw.gorecki93@gmail.com>
2023-06-10 09:46:04 +0200

    Change presentation shape

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -5,18 +5,18 @@ import { storyblokEditable } from '@storyblok/react';
 
 const Presentation = ({ blok }) => {
   return (
-    <div
-      className="grid grid-cols-1 md:grid-cols-5 gap-10 mt-8"
-      {...storyblokEditable(blok)}
-      data-aos="fade-right">
-      <div className="md:col-span-2">
-        <Image src={blok.image.filename} alt={blok.image.alt} loading="eager" />
-      </div>
+    <div {...storyblokEditable(blok)} className="mt-8 md:mt-16">
+      <h1 className="text-4xl md:text-5xl text-white">{blok.title}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-10 mt-8" data-aos="fade-right">
+        <div className="md:col-span-2">
+          <Image src={blok.image.filename} alt={blok.image.alt} loading="eager" />
+        </div>
 
-      <div
-        dangerouslySetInnerHTML={processRichText(blok.description)}
-        className={`text-2xl text-white md:col-span-3 description`}
-      />
+        <div
+          dangerouslySetInnerHTML={processRichText(blok.description)}
+          className={`text-2xl text-white md:col-span-3 description`}
+        />
+      </div>
     </div>
   );
 };
