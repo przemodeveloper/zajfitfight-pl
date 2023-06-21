a6f6ee1064e84af68d68592fd0c130ca2b0048b2
Przemek <przemyslaw.gorecki93@gmail.com>
2023-06-21 23:34:28 +0200

    Tweak contact

--- src/components/ContactBlock/ContactBlock.jsx
+++ src/components/ContactBlock/ContactBlock.jsx
@@ -1,8 +1,8 @@
 const ContactBlock = ({ email, phone }) => {
   return (
     <div className="flex justify-center items-center text-4xl md:text-5xl text-white flex-col md:flex-row">
-      <p className="md:border-r-4 md:pr-4">{email}</p>
-      <p className="md:pl-4">{phone}</p>
+      <p className="border-b-4 pb-4 md:border-b-0 md:pb-0 md:border-r-4 md:pr-4">{email}</p>
+      <p className="pt-4 md:pt-0 md:pl-4">{phone}</p>
     </div>
   );
 };
