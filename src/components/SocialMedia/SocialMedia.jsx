c06a78d5176a0e1a45b03d27152f68d52c153f8a
Przemek <przemyslaw.gorecki93@gmail.com>
2023-06-23 22:44:13 +0200

    Add aria-label

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -4,11 +4,11 @@ import { UilFacebook } from '@iconscout/react-unicons';
 const SocialMedia = () => {
   return (
     <div className="flex justify-center items-center gap-5 text-white">
-      <a href="https://www.instagram.com/zajfit_fight/">
+      <a href="https://www.instagram.com/zajfit_fight/" aria-label="instagram">
         <UilInstagram />
       </a>
 
-      <a href="https://www.facebook.com/profile.php?id=100088750616292">
+      <a href="https://www.facebook.com/profile.php?id=100088750616292" aria-label="facebook">
         <UilFacebook />
       </a>
     </div>
