56b04a28e18e9f8c0e05547a6f87f63952379154
Przemek <przemyslaw.gorecki93@gmail.com>
2024-01-07 14:29:24 +0100

    Change year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer
       className={`${styles.footer} bg-secondary py-4 flex justify-between flex-wrap px-8 border-primary border-t-4 text-base md:text-xl`}>
-      <p>© 2023 Zajfit&Fight, All Rights Reserved</p>
+      <p>© 2024 Zajfit&Fight, All Rights Reserved</p>
       <SocialMedia />
     </footer>
   );
